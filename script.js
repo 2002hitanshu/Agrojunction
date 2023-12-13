162e7cebed5c430e51f908cb4f8d8935fd9dc519
HITANSHU SINGH <122127354+2002hitanshu@users.noreply.github.com>
2023-12-13 19:32:38 +0530

    added script.js

---
+++ script.js
@@ -0,0 +1,30 @@
+let text = document.getElementById('text');
+let leaf = document.getElementById('leaf');
+let hill1 = document.getElementById('hill1');
+let hill4 = document.getElementById('hill4');
+let hill5 = document.getElementById('hill5');
+
+const scrollHandler = () => {
+    let value = window.scrollY;
+
+    const documentHeight = document.documentElement.scrollHeight;
+    const windowHeight = window.innerHeight;
+    const scrollableHeight = documentHeight - windowHeight;
+
+    if (value < scrollableHeight) {
+        const maxMarginTop = 850; 
+        const maxLeafTop = 500;
+        const maxLeafLeft = 500; 
+        const maxHillLeft = 500;
+
+        text.style.marginTop = Math.min(value * 2.5, maxMarginTop) + 'px';
+        leaf.style.top = Math.max(Math.min(value * -1.5, maxLeafTop), -maxLeafTop) + 'px';
+        leaf.style.left = Math.min(value * 1.5, maxLeafLeft) + 'px';
+        hill5.style.left = Math.min(value * 1.5, maxHillLeft) + 'px';
+        hill4.style.left = Math.max(Math.min(value * -1.5, maxHillLeft), -maxHillLeft) + 'px';
+        hill1.style.top = Math.min(value * 1.0, maxMarginTop) + 'px';
+    }
+};
+
+window.addEventListener('scroll', scrollHandler);
+
